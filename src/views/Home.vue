28ee3c629f9b107da435881ada42da763806bd30
antoniorestivo <nonebesides370@gmail.com>
2021-03-04 14:52:17 -0700

    My file

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="I am a Restivo this page is about me!!!" />
   </div>
 </template>
 
