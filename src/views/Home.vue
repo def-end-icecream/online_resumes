dfce9d5a021fa612fbf6d3bcb03c9ed7a46f4e75
nix <ngencokamin@gmail.com>
2021-03-08 09:41:09 -0800

    Adds starter code and dummy student data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>Casee was here</h1>
-    <HelloWorld msg="I am Charlotte this page is about me!!!" />
-  </div>
+  <div class="home"></div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      users: [
+        {
+          first_name: "Johnny",
+          last_name: "Test",
+          image_url:
+            "https://static.wikia.nocookie.net/johnnytest/images/1/1e/Johnny_large.png/revision/latest/scale-to-width-down/340?cb=20160509005837",
+        },
+        {
+          first_name: "Dexter",
+          last_name: "Laboratory",
+          profile_picture:
+            "https://static.wikia.nocookie.net/dexterslab/images/7/70/Dex_dexter_174x252.png/revision/latest/scale-to-width-down/340?cb=20150331204700",
+        },
+      ],
+    };
   },
+  created: function () {},
+  methods: {},
 };
 </script>
