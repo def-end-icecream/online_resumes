b742bb2a31bc376dd38bde590c8e4b67c3a2033e
nix <ngencokamin@gmail.com>
2021-03-08 09:50:58 -0800

    Displays user data in index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,37 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <div v-for="user in users" :key="user.id">
+      <h3>{{ user.first_name }} {{ user.last_name }}</h3>
+      <img :src="user.image_url" alt="User's image" style="width:25%" />
+      <button>More Info</button>
+    </div>
+  </div>
 </template>
 
 <style></style>
 
 <script>
 export default {
-  data: function () {
+  data: function() {
     return {
       users: [
         {
+          id: 1,
           first_name: "Johnny",
           last_name: "Test",
           image_url:
             "https://static.wikia.nocookie.net/johnnytest/images/1/1e/Johnny_large.png/revision/latest/scale-to-width-down/340?cb=20160509005837",
         },
         {
+          id: 2,
           first_name: "Dexter",
           last_name: "Laboratory",
-          profile_picture:
-            "https://static.wikia.nocookie.net/dexterslab/images/7/70/Dex_dexter_174x252.png/revision/latest/scale-to-width-down/340?cb=20150331204700",
+          image_url: "https://i.pinimg.com/originals/b4/51/89/b45189a152246f123bef2032444c7473.jpg",
         },
       ],
     };
   },
-  created: function () {},
+  created: function() {},
   methods: {},
 };
 </script>
