78d1894eeebe4b36107c770eb6b0f14158057f91
amoresca <casee.amores@gmail.com>
2021-03-08 15:21:46 -0500

    adds navbar to index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <nav class="navbar navbar-dark bg-primary fixed-top">
+      <span class="navbar-brand">Online Resumes</span>
+    </nav>
     <div v-for="user in users" :key="user.id">
       <h3>{{ user.first_name }} {{ user.last_name }}</h3>
       <img :src="user.image_url" alt="User's image" style="width:25%" />
@@ -11,8 +14,11 @@
   </div>
 </template>
 
-<style></style>
-
+<style scoped>
+body {
+  padding: 0;
+}
+</style>
 <script>
 export default {
   data: function() {
@@ -23,13 +29,14 @@ export default {
           first_name: "Johnny",
           last_name: "Test",
           image_url:
-            "https://static.wikia.nocookie.net/johnnytest/images/1/1e/Johnny_large.png/revision/latest/scale-to-width-down/340?cb=20160509005837",
+            "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages3.wikia.nocookie.net%2F__cb20120319144328%2Fjohnnytest%2Fimages%2F1%2F1e%2FJohnny_large.png",
         },
         {
           id: 2,
           first_name: "Dexter",
           last_name: "Laboratory",
-          image_url: "https://i.pinimg.com/originals/b4/51/89/b45189a152246f123bef2032444c7473.jpg",
+          image_url:
+            "https://i.pinimg.com/originals/b4/51/89/b45189a152246f123bef2032444c7473.jpg",
         },
       ],
     };
