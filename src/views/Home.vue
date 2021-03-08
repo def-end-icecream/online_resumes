328ca615deb4eb709d57606da9e45194a6851fd9
nix <ngencokamin@gmail.com>
2021-03-08 13:50:12 -0800

    Adds index axios request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="home">
-    <div v-for="user in users" :key="user.id">
-      <h3>{{ user.first_name }} {{ user.last_name }}</h3>
-      <img :src="user.image_url" alt="User's image" style="width:25%" />
+    <div v-for="student in students" :key="student.id">
+      <h3>{{ student.first_name }} {{ student.last_name }}</h3>
+      <img :src="student.image_url" alt="student's image" style="width:25%" />
       <br />
-      <router-link :to="`/users/${user.id}`">
+      <router-link :to="`/students/${student.id}`">
         <button>More Info</button>
       </router-link>
     </div>
@@ -14,27 +14,19 @@
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
   data: function() {
     return {
-      users: [
-        {
-          id: 1,
-          first_name: "Johnny",
-          last_name: "Test",
-          image_url:
-            "https://static.wikia.nocookie.net/johnnytest/images/1/1e/Johnny_large.png/revision/latest/scale-to-width-down/340?cb=20160509005837",
-        },
-        {
-          id: 2,
-          first_name: "Dexter",
-          last_name: "Laboratory",
-          image_url: "https://i.pinimg.com/originals/b4/51/89/b45189a152246f123bef2032444c7473.jpg",
-        },
-      ],
+      students: [],
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get("/api/students").then(response => {
+      console.log(response.data);
+      this.students = response.data;
+    });
+  },
   methods: {},
 };
 </script>
