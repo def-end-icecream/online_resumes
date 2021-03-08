e7a0b331b1dd14dd5afe1c899a67a66acddb9bab
amoresca <casee.amores@gmail.com>
2021-03-08 16:24:35 -0500

    adds student cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,21 @@
       <span class="navbar-brand">Online Resumes</span>
     </nav>
     <div class="container">
-      <div v-for="user in users" :key="user.id">
-        <h3>{{ user.first_name }} {{ user.last_name }}</h3>
-        <img :src="user.image_url" alt="User's image" style="width:25%" />
-        <br />
-        <router-link :to="`/users/${user.id}`">
-          <button>More Info</button>
-        </router-link>
+      <h1>Students</h1>
+      <div class="row row-cols-1 row-cols-md-3">
+        <div class="col mb-4" v-for="student in students" :key="student.id">
+          <div class="card">
+            <router-link :to="`/users/${student.id}`" class="stretched-link">
+              <img :src="student.image_url" class="card-img-top" alt="..."
+            /></router-link>
+            <div class="card-body">
+              <h5 class="card-title">
+                {{ student.first_name }} {{ student.last_name }}
+              </h5>
+              <h6 class="card-subtitle text-muted">Job Title</h6>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -19,26 +27,106 @@
 <style>
 body {
   padding: 0;
+  background-color: #eaeaea;
+}
+.home {
+  padding-top: 3.375rem;
 }
 </style>
 <script>
 export default {
   data: function() {
     return {
-      users: [
+      students: [
         {
-          id: 1,
-          first_name: "Johnny",
-          last_name: "Test",
+          last_name: "Amores",
+          first_name: "Casee",
+          email: "casee.amores@gmail.com",
+          phone_number: "111-1111",
+          short_bio:
+            "As an adept web developer with experience in a consulting environment, I understand that being a developer means being in a constant state of learning. I've fixed broken code that I inherited, deployed cloud servers for the first time by reading documentation, and learned Salesforce to support a client who used it. I am eager to dive further into backend development and overcome new challenges.",
+          linkedin_url: "linkedin.com/in/camores",
+          twitter_handle: "cassee twit",
+          personal_website_url: "n/a",
+          resume_url: "linkedin.com/in/camores",
+          github_url: "https://github.com/davidcalhoun4?tab=repositories",
           image_url:
-            "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fimages3.wikia.nocookie.net%2F__cb20120319144328%2Fjohnnytest%2Fimages%2F1%2F1e%2FJohnny_large.png",
+            "https://media-exp1.licdn.com/dms/image/C4E03AQHUGZ-q868W9A/profile-displayphoto-shrink_200_200/0/1572832092439?e=1620864000&v=beta&t=ZpD5706ZRVpQqajyMK_d4Ca-DGytu3JHDbwSK7wOt8c",
+          experience: [
+            {
+              student_id: 1,
+              start_date: "2021-01-01",
+              end_date: "2021-01-02",
+              job_title: "Job",
+              company_name: "company",
+              details: "work",
+            },
+          ],
         },
         {
-          id: 2,
-          first_name: "Dexter",
-          last_name: "Laboratory",
+          last_name: "Calhoun",
+          first_name: "David",
+          email: "calhoun5321@gmail.com",
+          phone_number: "222-2222",
+          short_bio:
+            "Budding full stack web developer with a passion for the arts and health.",
+          linkedin_url: "linkedin.com/in/davidcalhoun4",
+          twitter_handle: "david twit",
+          personal_website_url: "davidcalhoun4.github.io",
+          resume_url: "linkedin.com/in/davidcalhoun4",
+          github_url: "https://github.com/davidcalhoun4",
           image_url:
-            "https://i.pinimg.com/originals/b4/51/89/b45189a152246f123bef2032444c7473.jpg",
+            "https://media-exp1.licdn.com/dms/image/C4E03AQHsGMgLJHOJxw/profile-displayphoto-shrink_200_200/0/1611181156789?e=1620864000&v=beta&t=Rg9vCmUGaG0xSU5v_ic2hLJ20NeRABgJk9aunsPYUVE",
+          experience: [
+            {
+              student_id: 2,
+              start_date: "2021-01-03",
+              end_date: "2021-01-04",
+              job_title: "big time job",
+              company_name: "super duper company",
+              details: "workin hard",
+            },
+          ],
+        },
+        {
+          last_name: "Evans",
+          first_name: "Mackenzie",
+          email: "evans.mackenzie@gmail.com",
+          phone_number: "333-3333",
+          short_bio:
+            "Full stack web developer with a past life in labor and delivery/women's health nursing.",
+          linkedin_url: "https://www.linkedin.com/in/mackenzieevans1/",
+          twitter_handle: "mackenzie twit",
+          personal_website_url: "n/a",
+          resume_url: "https://www.linkedin.com/in/mackenzieevans1/",
+          github_url: "https://github.com/evansmackenzie",
+          image_url:
+            "https://media-exp1.licdn.com/dms/image/C4D03AQHFXFvCXjyc0Q/profile-displayphoto-shrink_200_200/0/1607708328167?e=1620864000&v=beta&t=BipzboHis6CWTKdZm8peTZnxhxdfMXcZLSqHqsyQqmg",
+          experience: [
+            {
+              student_id: 3,
+              start_date: "2021-01-05",
+              end_date: "2021-01-06",
+              job_title: "whatever",
+              company_name: "ya know",
+              details: "ugh",
+            },
+          ],
+        },
+        {
+          last_name: "Genco-Kamin",
+          first_name: "Nix",
+          email: "ngencokamin@gmail.com",
+          phone_number: "444-4444",
+          short_bio:
+            "Passionate full-stack web developer with an eye for detail",
+          linkedin_url: "https://www.linkedin.com/in/ngencokamin/",
+          twitter_handle: "nix twit",
+          personal_website_url: "ngencokamin.github.io",
+          resume_url: "https://www.linkedin.com/in/ngencokamin/",
+          github_url: "https://github.com/ngencokamin",
+          image_url:
+            "https://media-exp1.licdn.com/dms/image/C4D03AQEU9bojr3QH2g/profile-displayphoto-shrink_200_200/0/1609352591726?e=1620864000&v=beta&t=xGKu-4qvU_D4MGTVvxERzZVCcb8KAH98VKcjqONhPHE",
         },
       ],
     };
