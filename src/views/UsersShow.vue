e86861a8bedc22a8a0db5aae530f5c1f1630ae68
amoresca <casee.amores@gmail.com>
2021-03-10 14:39:22 -0500

    styles skills

--- src/views/UsersShow.vue
+++ src/views/UsersShow.vue
@@ -183,26 +183,13 @@
     <section class="resume-section" id="skills">
       <div class="resume-section-content">
         <h2 class="mb-5">Skills</h2>
-        <!-- <div class="subheading mb-3">Programming Languages & Tools</div> -->
-        <!-- <ul class="list-inline dev-icons">
-          <li class="list-inline-item"><i class="fab fa-html5"></i></li>
-          <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
-          <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
-          <li class="list-inline-item"><i class="fab fa-angular"></i></li>
-          <li class="list-inline-item"><i class="fab fa-react"></i></li>
-          <li class="list-inline-item"><i class="fab fa-node-js"></i></li>
-          <li class="list-inline-item"><i class="fab fa-sass"></i></li>
-          <li class="list-inline-item"><i class="fab fa-less"></i></li>
-          <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
-          <li class="list-inline-item"><i class="fab fa-gulp"></i></li>
-          <li class="list-inline-item"><i class="fab fa-grunt"></i></li>
-          <li class="list-inline-item"><i class="fab fa-npm"></i></li>
-        </ul> -->
-        <!-- <div class="subheading mb-3">Workflow</div> -->
-        <ul class="fa-ul mb-0">
-          <li v-for="skill in student.skills" :key="skill.id">
-            <span class="fa-li"><i class="fas fa-check"></i></span>
-            {{ skill.skill_name }}
+        <ul class="mb-0 list-inline">
+          <li
+            class="list-inline-item mb-2"
+            v-for="skill in student.skills"
+            :key="skill.id"
+          >
+            <span class="btn btn-primary">{{ skill.skill_name }}</span>
           </li>
         </ul>
       </div>
@@ -226,6 +213,15 @@
 img {
   border-radius: 50%;
   width: 150px;
+}
+#skills .btn-primary:hover {
+  background-color: #bd5d38;
+  border-color: #bd5d38;
+  cursor: default;
+}
+#skills .list-inline-item:nth-child(even) .btn {
+  background-color: #6c757d;
+  border-color: #6c757d;
 }
 </style>
 
